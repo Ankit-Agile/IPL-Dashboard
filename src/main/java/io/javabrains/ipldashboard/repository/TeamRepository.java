b1cdba4e38098986ba8bb1a5c768fee576bfcc5d
Ankit Kumar <ankitsingh310118@gmail.com>
2021-07-27 01:33:33 +0530

    Complete Project

---
+++ src/main/java/io/javabrains/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package io.javabrains.ipldashboard.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.javabrains.ipldashboard.model.Team;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+    
+    Team findByTeamName(String teamName);
+}
